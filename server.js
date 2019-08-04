4568c7a02334718931f2a6fc7e8571424344324a
iasminaedina <iasmina.edina@gmail.com>
2019-08-04 19:21:18 +0200

    cleanup

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-app.use(express.static("app")); // myApp will be the same folder name.
+app.use(express.static("app"));
 app.get('/', function (req, res,next) {
  res.redirect('/'); 
 });
