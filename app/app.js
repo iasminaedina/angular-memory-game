d319a5fd627adeadf6e3fde89ed68263e9539cd4
iasminaedina <iasmina.edina@gmail.com>
2019-08-04 19:19:54 +0200

    update

--- app/app.js
+++ app/app.js
@@ -27,18 +27,9 @@ var memoryGame = angular.module('memoryGame', ['ngAnimate', 'ngRoute'])
   .controller('winnerCtrl', function winnerCtrl($scope) {
     $scope.message = "YOU WIN!";
   })
-
-  //usages:
-  //- in the repeater as: <mg-card tile="tile"></mg-card>
-  //- card currently being matched as: <mg-card tile="game.firstPick"></mg-card>
-
   .directive('mgCard', function() {
     return {
       restrict: 'E',
-      // instead of inlining the template string here, one could use templateUrl: 'mg-card.html'
-      // and then either create a mg-card.html file with the content or add
-      // <script type="text/ng-template" id="mg-card.html">.. template here.. </script> element to
-      // index.html
       template: '<div class="container">' +
                   '<div class="card" ng-class="{flipped: tile.flipped}">' +
                     '<img class="front" ng-src="img/back.png">' +
